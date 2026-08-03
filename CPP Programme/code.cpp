6a34a236cf9fed3dd32e971ba3e41da91e9315c6
Fabiha Jannat <burcinkiabey@gmail.com>
2026-08-03 20:57:31 +0600

    code.cpp

---
+++ CPP Programme/code.cpp
@@ -0,0 +1,13 @@
+#inclue <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    char grade = "a" ;
+    float PI = 3.14f ;
+
+    bool isSafe = true;
+    
+    cout << isSafe << endl;
+    return 0;
+}                
