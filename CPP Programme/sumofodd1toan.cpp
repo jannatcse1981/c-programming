bd22afab2519b5610ad1521ee99110604340a66f
Fabiha Jannat <burcinkiabey@gmail.com>
2026-08-04 21:32:20 +0600

    sumofodd1toan.cpp

--- CPP Programme/sumofodd1toan.cpp
+++ CPP Programme/sumofodd1toan.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main() {
     int n = 5;
+    int odsum = 0;
     //print odd numbers from 1 to n;
     for ( int i = 1; i<= n; i++) {
           if ( i%2 != 0) {
                 cout << i << endl;
+                odsum += i;
           }
 
 
 
     }
+    cout << "Sum of odd numbers: " << odsum << endl;
     return 0;
 }        
