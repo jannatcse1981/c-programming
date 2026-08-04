d838aec2d1ac45725be6b39793ea3cbb11d7b93b
Fabiha Jannat <burcinkiabey@gmail.com>
2026-08-04 21:23:49 +0600

    sumofodd1toan.cpp

---
+++ CPP Programme/sumofodd1toan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    //print odd numbers from 1 to n;
+    for ( int i = 1; i<= n; i++) {
+          if ( i%2 != 0) {
+                cout << i << endl;
+          }
+
+
+
+    }
+    return 0;
+}        
