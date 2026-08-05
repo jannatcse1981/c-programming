c4314861d6b311c2c23ed5222099622556016c85
Fabiha Jannat <burcinkiabey@gmail.com>
2026-08-05 22:38:56 +0600

    pattern7.cpp

---
+++ CPP Programme/pattern7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 7;
+    for(int i = 0; i<n; i++) {
+        
+        for(int j = 0; j<n+1; j++) {
+            cout << "*";
+
+        }
+        cout << endl;
+    }    
+    return 0;
+}
+   
